198c12e26affc0c2aa0741aa34190afafd9fc3cd
Mark Cinco <Mark Cinco>
2016-06-14 00:50:46 +0800

    Reduse images filesize

--- js/waveside.js
+++ js/waveside.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
-	if ($(window).width() < 769) {
-		console.log("less than 769");
-	   $('#nv').removeClass('collapse navbar-collapse');
-		$('#nv').addClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
-	}
-	else {
-	   console.log('More than 768');
-	   $('#nv').removeClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
-		$('#nv').addClass('collapse navbar-collapse');
-	}
+$(document).ready(function () {
+    if ($(window).width() < 769) {
+        console.log("less than 769");
+        $('#nv').removeClass('collapse navbar-collapse');
+        $('#nv').addClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
+    }
+    else {
+        console.log('More than 768');
+        $('#nv').removeClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
+        $('#nv').addClass('collapse navbar-collapse');
+    }
 });
 
-$(window).resize(function(){
+$(window).resize(function () {
     if ($(window).width() < 769) {
-		console.log("less than 769");
-	   $('#nv').removeClass('collapse navbar-collapse');
-		$('#nv').addClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
-	}
-	else {
-	   console.log('More than 768');
-	   $('#nv').removeClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
-		$('#nv').addClass('collapse navbar-collapse');
-	}
+        console.log("less than 769");
+        $('#nv').removeClass('collapse navbar-collapse');
+        $('#nv').addClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
+    }
+    else {
+        console.log('More than 768');
+        $('#nv').removeClass('navmenu navmenu-default navmenu-fixed-left offcanvas');
+        $('#nv').addClass('collapse navbar-collapse');
+    }
 });
 
 var elementOffset = $('#waveNav').offset().top;
 
-		var dest = elementOffset;
+var dest = elementOffset;
 
-$('#waveNav').affix({offset: dest });
-$('#minlogo').affix({offset: dest });
+$('#waveNav').affix({offset: dest});
+$('#minlogo').affix({offset: dest});
