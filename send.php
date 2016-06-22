6bba55cc087d3ba69be74a53471387936d66fce5
Kaira <livingbetweenrainbows@gmail.com>
2016-06-22 13:10:01 +0800

    More modified php

--- send.php
+++ send.php
@@ -3,8 +3,19 @@ if($_POST){
     $name = $_POST['cName'];
     $email = $_POST['cMail'];
     $message = $_POST['cMess'];
+	
+	$message2 = <<<EMAIL
+	
+	Name of Customer $name
+	
+	Email of customer $email
+	
+	Message
+	$message
+
+EMAIL;
 
 //send email
-    mail("livingbetweenrainbows@gmail.com", "Wave Query",$email, $message);
+    mail("livingbetweenrainbows@gmail.com", "Wave Query",$message,$email);
 }
 ?>
