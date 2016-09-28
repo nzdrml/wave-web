6def985bbe342e9ebe548882851534c0303a81b4
Mark Cinco <mark5cinco92@gmail.com>
2016-09-28 23:59:34 +0900

    Update register

--- app/modules/auth/auth.js
+++ app/modules/auth/auth.js
@@ -2,7 +2,7 @@
  * Created by mark5 on 24/07/2016.
  */
 
-angular.module("auth", ["AuthService"])
+angular.module("auth", ["AuthService", "AddressService", "home-directive"])
     .config(function ($stateProvider) {
         $stateProvider
             .state("register", {
@@ -16,7 +16,7 @@ angular.module("auth", ["AuthService"])
                 controller: "LoginCtrl as login"
             })
     })
-    .controller("RegisterCtrl", function ($q, $scope, $state, $ocLazyLoad, Restangular, growl, Auth) {
+    .controller("RegisterCtrl", function ($q, $scope, $state, $ocLazyLoad, Restangular, growl, Auth, Address) {
         var vm = this;
 
         vm.form = {
@@ -30,12 +30,32 @@ angular.module("auth", ["AuthService"])
 
             vm.isPosting = true;
 
+            Auth.logout();
             Auth.requestToken()
                 .then(function (accessData) {
                     console.log(accessData);
-                    return Restangular.all('users').post({user: vm.form}, null, {
+                    vm.form.user.company = "Company";
+                    return Restangular.all('users').post({user: vm.form.user}, null, {
                         Authorization: "Bearer " + accessData
                     });
+                })
+                .tap(function(userResponse){
+                    console.log("User Response: " + userResponse);
+
+                    var defaultInfo = {
+                        user_id: userResponse.id,
+                        country: "Philippines",
+                        coordinates: ""
+                    };
+
+                    var addresses = {
+                        home: _.extend(vm.form.home, defaultInfo, {type: "home"}),
+                        work: _.extend(vm.form.work, defaultInfo, {type: "work"})
+                    };
+
+                    console.log("Sending: " + addresses);
+
+                    return $q.join(Address.service.post({address: addresses.home}), Address.service.post({address: addresses.work}));
                 })
                 .then(function (response) {
                     growl.success("Successfully Registered! Redirecting to login in...", {
